bb4ba048f114fb9f39b4430844c163bad2ecfe85
Lenard <bebobhd@gmail.com>
2023-07-15 18:03:27 +0100

    prototype for hitting the api

--- app/pyfall/src/api.py
+++ app/pyfall/src/api.py
@@ -2,11 +2,25 @@ import requests
 from handlers import *
 
 # template code for apicall
-respons = requests.get('https://randomuser.me/api')
-print(respons.status_code)
+# respons = requests.get('https://randomuser.me/api')
+# print(respons.status_code)
 
-result = respons.json()['results'][0]
-name = f"{result['name']['first']} {result['name']['last']}"
+# result = respons.json()['results'][0]
+# name = f"{result['name']['first']} {result['name']['last']}"
 
-print(name)
-print(links())
+# print(name)
+# print(links())
+
+# will return the 
+def generic_request():
+    url = links(route=routes["random"])
+    response = requests.get(url)
+    result = response.json()
+    return result
+
+def specific_request(url):
+    response = requests.get(url)
+    result = response.json()
+    return result
+
+print(generic_request())
