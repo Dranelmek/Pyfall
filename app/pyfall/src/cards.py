14d968af73d350dd47c9567fc86a155d80634643
Lenard <bebobhd@gmail.com>
2023-07-17 15:04:06 +0100

    additions to the cards file

--- app/pyfall/src/cards.py
+++ app/pyfall/src/cards.py
@@ -1,21 +1,31 @@
-# TODO: create card class that acts as a template for card information the user will see
-
-# TODO: make card class able to populate itsself and abstract api calls
 from errors import *
 
 class Card:
     '''
     The card class is for generating lightweight card
-    objects with the necesary data on magic cards. This
-    is a superclass for generic cards, each card type will
-    be its own class.
+    objects with the necesary data on magic cards. 
+    depending on the card type certain values will be set to none
     '''
+    # TODO: finish init function and make it set the object properties
+
     def __init__(self, data:dict):
         try:
+            # generic values
             self.name
             self.cost
             self.cmc
             self.rulesText
+            self.cardTypes
+            self.flavorText
+            self.color
+
+            # type specific values
+            self.p
+            self.t
+            self.loyalty
+            self.defense
+
+
         except ApiError as a:
             print(a)
         except:
